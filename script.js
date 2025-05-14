abe2cdbf9958d703be9ebb2a08ade3525b315e11
Daria Vetrykush <dariavetrykush@gmail.com>
2025-05-14 23:17:18 +0300

    change average sound again

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     let average = sum / bufferLength;
 
-    return average > 50;
+    return average > 55;
   }
 
   function blowOutCandles() {
